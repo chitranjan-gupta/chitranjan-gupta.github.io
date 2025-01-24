6669223e76ef0cd4d64b8d92151f624f2a8e2794
Chitranjan Gupta <37315561+chitranjan-gupta@users.noreply.github.com>
2025-01-24 19:21:22 +0530

    adde contact form submission using notion (#2)

--- components/contact-form.tsx
+++ components/contact-form.tsx
@@ -1,47 +1,43 @@
 "use client";
 
-import { useRef } from "react";
+import { z } from "zod";
+import { createForm } from "@/lib";
 import type { FormEvent } from "react";
 
-export default function ContactForm() {
-  const first_name = useRef<HTMLInputElement>(null),
-    middle_name = useRef<HTMLInputElement>(null),
-    last_name = useRef<HTMLInputElement>(null),
-    email = useRef<HTMLInputElement>(null),
-    phone_number = useRef<HTMLInputElement>(null),
-    input_file = useRef<HTMLInputElement>(null),
-    input_message = useRef<HTMLTextAreaElement>(null);
+const UserSchema = z.object({
+  first_name: z.string().min(1, 'First name is required'),
+  middle_name: z.string().optional(),
+  last_name: z.string().min(1, 'Last name is required'),
+  email: z.string().email('Invalid email address'),
+  phone_number: z.string().regex(/^[0-9]{10}$/, 'Invalid phone number'),
+  messages: z.string().optional(),
+});
 
-  function validate(event: FormEvent<HTMLFormElement>) {
+export default function ContactForm() {
+  const handleSubmit = async (event: FormEvent) => {
     event.preventDefault();
-    if (first_name?.current?.value?.length! <= 0) {
-      alert("Fill the first name");
-      return false;
-    }
-    if (middle_name?.current?.value?.length! <= 0) {
-      alert("Fill the middle name");
-      return false;
-    }
-    if (last_name?.current?.value?.length! <= 0) {
-      alert("Fill the last name");
-      return false;
-    }
-    if (email?.current?.value.length! <= 0) {
-      alert("Fill the email name");
-      return false;
-    }
-    if (input_message?.current?.value.length! <= 0) {
-      alert("Fill your message");
-      return false;
+  
+    const formData = new FormData(event.target as HTMLFormElement);
+    const data = Object.fromEntries(formData.entries());
+  
+    try {
+      const parsedData = UserSchema.parse(data);
+      await createForm(parsedData)
+      // Submit the data to your API or perform other actions
+    } catch (error) {
+      if (error instanceof z.ZodError) {
+        console.log(error.issues);
+        // Display the validation errors to the user
+      } else {
+        console.error(error);
+      }
     }
   }
-
   return (
-    <form onSubmit={validate} className="flex flex-col p-[20px] pt-0">
+    <form onSubmit={handleSubmit} className="flex flex-col p-[20px] pt-0">
       <div className="flex flex-col md:flex-row justify-between items-center p-[10px] gap-y-2 md:gap-y-0 gap-x-0 md:gap-x-2">
         <input
           className="w-full md:w-[280px] h-[50px] shadow-md border-none rounded-[5px] outline-none bg-white dark:bg-[#E0E0E0] indent-[10px]"
-          ref={first_name}
           id="first_name"
           name="first_name"
           type="text"
@@ -49,7 +45,6 @@ export default function ContactForm() {
         />
         <input
           className="w-full md:w-[280px] h-[50px] shadow-md border-none rounded-[5px] outline-none bg-white dark:bg-[#E0E0E0] indent-[10px]"
-          ref={middle_name}
           id="middle_name"
           name="middle_name"
           type="text"
@@ -57,7 +52,6 @@ export default function ContactForm() {
         />
         <input
           className="w-full md:w-[280px] h-[50px] shadow-md border-none rounded-[5px] outline-none bg-white dark:bg-[#E0E0E0] indent-[10px]"
-          ref={last_name}
           id="last_name"
           name="last_name"
           type="text"
@@ -67,7 +61,6 @@ export default function ContactForm() {
       <div className="flex flex-col md:flex-row justify-between items-center p-[10px] gap-y-2 md:gap-y-0 gap-x-0 md:gap-x-2">
         <input
           className="w-full md:w-[280px] h-[50px] shadow-md border-none rounded-[5px] outline-none bg-white dark:bg-[#E0E0E0] indent-[10px]"
-          ref={email}
           id="email"
           name="email"
           type="email"
@@ -75,7 +68,6 @@ export default function ContactForm() {
         />
         <input
           className="w-full md:w-[280px] h-[50px] shadow-md border-none rounded-[5px] outline-none bg-white dark:bg-[#E0E0E0] indent-[10px]"
-          ref={phone_number}
           id="phone_number"
           name="phone_number"
           type="tel"
@@ -84,7 +76,6 @@ export default function ContactForm() {
         <span className="w-full md:w-[260px] h-[50px] shadow-md border-none outline-none rounded-[5px] bg-white dark:bg-[#E0E0E0] p-[5px]">
           <input
             className="w-[210px] h-[30px] md:h-[25px] shadow-none border-none rounded-bl-[0] rounded-br-[0] outline-none bg-white dark:bg-[#E0E0E0] indent-[10px]"
-            ref={input_file}
             id="input_file"
             name="input_file"
             type="file"
@@ -96,9 +87,8 @@ export default function ContactForm() {
       <div className="flex flex-col md:flex-row justify-between items-center p-[10px] gap-x-0 md:gap-x-2">
         <textarea
           className="w-full md:w-full h-[50px] pt-[10px] pl-[10px] shadow-md border-none rounded-[5px] outline-none bg-white dark:bg-[#E0E0E0] indent-[0px]"
-          ref={input_message}
-          id="input_message"
-          name="input_message"
+          id="messages"
+          name="messages"
           placeholder="Your Message"
         ></textarea>
       </div>
