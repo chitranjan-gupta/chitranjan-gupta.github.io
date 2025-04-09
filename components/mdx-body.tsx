494a5db658e24310f41a1ff696b0afa3de7c7659
Chitranjan Gupta <37315561+chitranjan-gupta@users.noreply.github.com>
2025-04-09 07:18:35 +0000

    updated blogs and added youtube embed in markdown

--- components/mdx-body.tsx
+++ components/mdx-body.tsx
@@ -1,3 +1,4 @@
+import { ReactNode } from "react";
 import { MDXRemote } from "next-mdx-remote/rsc";
 import remarkGfm from "remark-gfm";
 import rehypeSlug from "rehype-slug";
@@ -5,12 +6,92 @@ import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import remarkA11yEmoji from "@fec/remark-a11y-emoji";
 import remarkToc from "remark-toc";
 import { remarkCodeHike, recmaCodeHike } from "codehike/mdx";
-import { MDXComponents } from "@/components/mdx-components";
 import { Pre, RawCode, highlight } from "codehike/code";
+import Link from "next/link";
+import { YouTubeEmbed } from "@next/third-parties/google";
+import Image, { ImageProps } from "next/image";
+import { Node, Parent } from 'unist';
+
+interface LinkNode extends Node {
+  type: 'link';
+  url: string;
+  title: string;
+  children: Node[];
+}
+
+const remarkUnwrapAnchors = () => {
+  return (tree: Parent) => {
+    tree.children.forEach((node) => {
+      if (node.type === 'paragraph' && (node as Parent).children.length === 1 && (node as Parent).children[0].type === 'link') {
+        const linkNode = (node as Parent).children[0] as LinkNode;
+        node.type = 'link';
+        (node as any).url = linkNode.url;
+        (node as any).title = linkNode.title;
+        (node as any).children = linkNode.children;
+      }
+    });
+  };
+};
+
 
 async function Code({ codeblock }: { codeblock: RawCode }) {
   const highlighted = await highlight(codeblock, "github-from-css");
   return <Pre code={highlighted} style={highlighted.style} />;
+}
+
+interface AProps extends React.DetailedHTMLProps<React.AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement> {
+  children?: ReactNode; // Make children optional
+}
+
+function A({ children, ...props }: AProps) {
+  if (
+    props.href?.match(
+      /^https?:\/\/(?:www\.)?youtube\.com\/watch\?v=([a-zA-Z0-9_-]{11})$/
+    )
+  ) {
+    return (
+      <div>
+        <YouTubeEmbed
+          videoid={props.href.split("v=")[1]}
+          height={400}
+          params="controls=0"
+        />
+      </div>
+    );
+  } else {
+    return (
+      <Link {...props} href={props.href || ""}>
+        {children}
+      </Link>
+    );
+  }
+}
+
+interface ImgProps extends React.DetailedHTMLProps<React.ImgHTMLAttributes<HTMLImageElement>, HTMLImageElement> {}
+
+function Img(props: ImgProps) {
+  if (
+    typeof props.src === 'string' &&
+    props.src?.match(
+      /^https?:\/\/(?:i\.)?img\.youtube\.com\/vi\/([a-zA-Z0-9_-]{11})\/([0-9]+)\.(jpg|jpeg|png)$/
+    )
+  ) {
+    return <></>;
+  } else {
+    return (
+      <Image
+        sizes="100vw"
+        style={{
+          width: '100%',
+          height: 'auto',
+        }}
+        width={500}
+        height={300}
+        {...(props as ImageProps)}
+        alt={props.alt || ""}
+      />
+    );
+  }
 }
 
 const chConfig = {
@@ -31,13 +112,14 @@ export function MDXBody({ children }: { children: string }) {
             // generates a table of contents based on headings
             remarkToc,
             [remarkCodeHike, chConfig],
+            remarkUnwrapAnchors
           ],
           // These work together to add IDs and linkify headings
           rehypePlugins: [rehypeSlug, rehypeAutolinkHeadings],
           recmaPlugins: [[recmaCodeHike, chConfig]],
         },
       }}
-      components={{ Code, ...MDXComponents }}
+      components={{ Code, a: A, img: Img }}
     />
   );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -14,6 +14,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "komarev.com",
       },
+      {
+        protocol: "https",
+        hostname: "github.com",
+      },
     ],
   },
 };
